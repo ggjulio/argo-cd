e47297e517dcb6e2f77ef427afd3c065bf6b6682
Julio <gonzalez.julio8959@gmail.com>
2024-10-04 19:19:48 +0000

    try lint tools

--- hack/installers/install-lint-tools.sh
+++ hack/installers/install-lint-tools.sh
@@ -2,6 +2,6 @@
 set -eux -o pipefail
 
 # renovate: datasource=gomod packageName=golangci/golangci-lint
-GOLANGCI_LINT_VERSION=1.61.0
+GOLANGCI_LINT_VERSION=1.60.0
 
 GO111MODULE=on go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}"
